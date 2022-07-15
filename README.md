2f5b5b3f43e4ed3c659bf9f7b90d950247c57df4
ardhan <ardhan.s.pratama@gmail.com>
2022-07-15 21:32:46 +0800

    Added WebSocketConfig

--- README.md
+++ README.md
@@ -13,14 +13,14 @@ https://docs.spring.io/spring-kafka/docs/current/reference/html/
 1. Start Kafka zookeeper (on a new terminal shell)
 
 **bin/zookeeper-server-start.sh config/zookeeper.properties**
-(for windows)
-**.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties**
+
+**.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties** (for windows)
 
 2. Start Kafka server (on a new terminal shell)
 
 **bin/kafka-server-start.sh config/server.properties**
-(for windows)
-**.\bin\windows\kafka-server-start.bat .\config\server.properties**
+
+**.\bin\windows\kafka-server-start.bat .\config\server.properties** (for windows)
 
 3. Clone the repository using IntelliJ IDE 
 
--- src/main/java/com/wareeyes/app/kafka/KafkaConsumer.java
+++ src/main/java/com/wareeyes/app/kafka/KafkaConsumer.java
@@ -3,12 +3,20 @@ package com.wareeyes.app.kafka;
 import com.wareeyes.app.userlogin.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+
+// TODO: add functionality for consumer to convert and send data to websocket
 @Service
 public class KafkaConsumer {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
     private KafkaTemplate<String, User> kafkaTemplate;
 
